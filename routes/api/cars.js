8fefbb5e089759c575a9943a09788493a1ca3958
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-06-29 21:47:49 -0300

    funciona 1,2,3 - ver si reformular

--- routes/api/cars.js
+++ routes/api/cars.js
@@ -5,57 +5,29 @@ const { json } = require('body-parser');
 
 // para consumir otra api neseito instalar request
 // npm install request 
-//  DEBUG=api-car:* npm start 
 
-// punto 1
-// car_id 17491, 17492 y 17493
-/**
- * 17491
- * 
- * {
-    "car_id": 17491,
-    "_id": "5e14f581bf0d686228f6436a",
-    "mlid": "MLA851699685"
-}
- * 
- * 
- * 1792
- * 
- * {
-    "car_id": 17492,
-    "_id": "5e14f581bf0d686228f6436b",
-    "mlid": "MLA851699686"
-}
- * 
- * 
- * 
- * {
-    "car_id": 17493,
-    "_id": "5e14f581bf0d686228f6436c",
-    "mlid": "MLA851699687"
-}
- * 
- * 
- */
-
-
-
-let url1 = 'https://al3xcr91.pythonanywhere.com/test/api/v1/site/'
+let url1 = 'https://al3xcr91.pythonanywhere.com/test/api/v1/site/';
+let url2 = 'https://al3xcr91.pythonanywhere.com/test/api/v1/site-by-id/';
 let url3 = 'https://al3xcr91.pythonanywhere.com/test/api/v1/meli-api/';
 let url4 = 'https://al3xcr91.pythonanywhere.com/test/api/v1/meli/'
 
-////--------------------- punto 1 ---------------------
 
+////--------------------- punto 1 ---------------------
+// 17491, 17492 y 17493
 // verifico que el car_id exista en ERP OK
 router.get('/:car_id', function(req, res){
+    
     const { car_id } = req.params;
     
     request( `${url1}/${car_id} `, function(err, body){
 
-        const data = JSON.parse(JSON.stringify(body.body));
+        const data = JSON.parse(body.body);
+        console.log(`primer data: ${JSON.stringify(data)}`)
 
-        if(!data.car_id) {
+        if(data) {
             if(car_id == data.car_id) {
+                data1 = data;
+                console.log(`primer data1: ${JSON.stringify(data1)}`)
                 res.status(200).json({
                     data: data,
                     message: 'ok'
@@ -67,67 +39,87 @@ router.get('/:car_id', function(req, res){
                 });
             }
         }
-
-
     });
 
 });
 
-////--------------------- punto 2 ---------------------
+////--------------------- punto 5 ---------------------
 
 // actualizar codigo segun si existe
-router.put('/:card_id', function(req, res){
+router.put('ambos/:car_id', function(req, res){
 
-    const { car_id } = req.params;
-    // const { body: car } = req; 
-    const card_id_existe = JSON.parse( JSON.stringify( verifcarCardId(url1, car_id) ) );
+    const { car_id } = req.params; 
+    const { body: car } = req; // datos a actualizar
+    
+    console.log(`segundo car: ${JSON.stringify(car)}`)
+    console.log(`segundo car: ${car_id}`) 
 
-    if(card_id_existe === 'error') {
-        let id_interno = card_id_existe.data._id;
-        res.status(200).json({
-            data: id_interno,
-            message: 'ok'
-        });
-    } else {
-        res.status(200).json({
-            data: car_id,
-            message: 'error'
-        });
-    }
-
-    // No se pudo probar
-    request({
-            url: `https://al3xcr91.pythonanywhere.com/test/api/v1/site-by-id/${id_interno}/`, //URL to hit
-            // qs: {from: 'example', time: +new Date()}, //Query string data
-            method: 'PUT', // specify the request type
-            headers: { // speciyfy the headers
+    // verifico que el card_id exista con el primer endpoint
+    request( {url: `http://localhost:3000/api/cars/${car_id}`, 
+              method: 'GET',
+              headers: { 
                 'Content-Type': 'application/json',
             },
-            body: car //Set the body as a string
-            }, function(error, response, body){
+        }, function(err, body){
+            // si existe 
+            const data = JSON.parse(body.body);
+            console.log(`primer data service 1: ${JSON.stringify(data)}`)
 
-                const data = JSON.parse(JSON.stringify(body.body));
+            if( data.message == "ok" ) {
+                console.log(`ingreso a ok`);
+                console.log(`primer data service get: ${JSON.stringify(data.data)}`)
 
-                if(error) {
-                    console.log(error);
-                } else {
-                    console.log(response.statusCode, body);
-                }
-            }
-    );
+                console.log(data.data._id)
+                // voy a actualizar
+                request({
+                    url: `${url2}${data.data._id}/`, 
+                    method: 'PUT', 
+                    headers: { 
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(car)
+                    }, function(error, body){
+                        // manejo el error de actualizacion de la data
+                        const data = JSON.parse(body.body);
+                        console.log(`primer data service put: ${JSON.stringify(data)}`)
 
+                        if(!data.Error_msg) {
+                            // si no se producjo ningun error pasa al endpoint 3 para actuallizar meli
+                            
+
+                            res.status(200).json({
+                                data: data,
+                                message: 'ok'
+                            });
+                        } else {
+                            res.status(200).json({
+                                data: data,
+                                message: 'error'
+                            });
+                        }
+                    }
+                );
+
+            } else {
+                console.log(`paso un error`)
+            }      
+    });
 
 });
 
+router.put('/:car_id', function(req, res){
+
+});
 
 ////--------------------- punto 3 ---------------------
 
 router.get('/meli/:item_id', function(req, res){
     const { item_id } = req.params;
-    
+
+
     request( `${url3}/${item_id} `, function(err, body){
 
-        const data = JSON.parse(JSON.stringify(body.body));
+        const data = JSON.parse(body.body);
 
         if(data) {
             res.status(200).json({
@@ -152,43 +144,30 @@ router.put('/meli/:item_id', function(req, res){
     const { body: car } = req; 
     
     request({
-        url: `${url4}/${id_interno}/`, //URL to hit
-        // qs: {from: 'example', time: +new Date()}, //Query string data
-        method: 'PUT', // specify the request type
-        headers: { // speciyfy the headers
+        url: `${url4}/${id_interno}/`, 
+        method: 'PUT', 
+        headers: { 
             'Content-Type': 'application/json',
         },
-        body: car //Set the body as a string
-        }, function(error, response, body){
+        body: JSON.parse(car)
+        }, function(error, body){
 
-            // const data = JSON.parse(JSON.stringify(body.body));
-
-            if(error) {
-                console.log(error);
+            const data = JSON.parse(body.body);
+            if(data) {
+                res.status(200).json({
+                    data: JSON.parse(data),
+                    message: 'ok'
+                });
             } else {
-                console.log(response.statusCode, body);
+                res.status(200).json({
+                    data: JSON.parse(data),
+                    message: 'error'
+                });
             }
+
         }
-);
+    );
 
 });
-
-/// Helpers
-
-const verifcarCardId = (url, car_id) => {
-    request( `${url}/${car_id} `, function(err, body){
-
-        const data = JSON.parse(JSON.stringify(body.body));
-
-        if(!data.car_id) {
-            if(car_id == data.car_id) {
-                return data;
-                
-            } else {
-                return 'error';
-            }
-        }
-    });
-}
 
 module.exports = router;
